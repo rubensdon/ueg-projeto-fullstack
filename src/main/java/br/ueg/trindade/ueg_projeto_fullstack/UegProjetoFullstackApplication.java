542ce2601c0334ae1bae3d1ac6f3db2674637086
unknown <rubens33.rj@gmail.com>
2026-08-21 22:37:57 -0300

    criação do projeto

---
+++ src/main/java/br/ueg/trindade/ueg_projeto_fullstack/UegProjetoFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.ueg_projeto_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UegProjetoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UegProjetoFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/trindade/ueg_projeto_fullstack/UegProjetoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.ueg_projeto_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UegProjetoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
